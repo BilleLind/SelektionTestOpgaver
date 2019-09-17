ac70ad1545f89be7979f37fbf81aa6ac06e3653e
yokarak <ande67f3@edu.easj.dk>
2019-09-17 10:30:49 +0200

    opgave 3

---
+++ src/Opgave3.java
@@ -0,0 +1,12 @@
+public class Opgave3 {
+        public static String størst(int first, int second){
+            String result = "second";
+
+            if (first < second) {
+                result ="first ";
+            }
+            return  result;
+        }
+
+
+}
--- src/Opgave3Test.java
+++ src/Opgave3Test.java
@@ -1,3 +1,36 @@
 public class Opgave3Test {
+    public static void main(String[] args) {
+
+        int first;
+        int second;
+
+        //test 3.1
+        first =20;
+        second =35;
+        if (Opgave3.størst(first, second).equals("second")) {
+            System.out.println("pass");
+        }else {System.out.println("fail");}
+
+        //test 3.2
+        first = 100;
+        second = 10;
+
+        if (Opgave3.størst(first, second).equals("first")) {
+            System.out.println("pass");
+        } else {System.out.println("fail");}
+
+        //test 3.3
+        first=5;
+        second=10;
+
+        if (Opgave3.størst(first, second).equals("second")) {
+            System.out.println("pass");
+        } else { System.out.println("fail");}
+
+
+
+
+
+    }
 
 }
