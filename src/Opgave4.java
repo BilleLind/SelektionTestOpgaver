924d1d88c442f29578cbdad00dda26f4febf8008
yokarak <ande67f3@edu.easj.dk>
2019-09-17 10:44:31 +0200

    opgaver 4 4.1 4.2 4.3

---
+++ src/Opgave4.java
@@ -0,0 +1,11 @@
+public class Opgave4 {
+    public static String størst1(int first, int second, int third) {
+        String result ="third";
+                if (first > second && first > third) {
+                    System.out.println("first");
+                } else if (second > first && second > third) {
+                    System.out.println("second");
+                }
+                    return result;
+    }
+}
---
+++ src/Opgave4Test.java
@@ -0,0 +1,32 @@
+public class Opgave4Test {
+    public static void main(String[] args) {
+        int first;
+        int second;
+        int third;
+
+        //4.1
+        first= 10;
+        second=10;
+        third=11;
+        if (Opgave4.størst1(first, second, third).equals("third")) {
+            System.out.println("pass");
+        } else {System.out.println("fail");}
+
+        //4.2
+        first=12;
+        second=15;
+        third=1;
+        if (Opgave4.størst1(first, second, third).equals("second")) {
+            System.out.println("pass");
+        } else {System.out.println("fail");}
+
+        //4.3
+        first=1000;
+        second=2501;
+        third=999;
+        if (Opgave4.størst1(first, second, third).equals("first")) {
+            System.out.println("pass");
+        } else {System.out.println("fail");}
+
+    }
+}
